95f04193a1cecf7918ef6478504d65de9a1a6cad
Mandar Kulkarni <mandar242@gmail.com>
2025-07-22 12:35:08 -0700

    initialize collection with ansible-creator

---
+++ tests/unit/test_basic.py
@@ -0,0 +1,10 @@
+"""Unit tests for hashicorp.vault."""
+
+
+def test_basic() -> None:
+    """Dummy unit test that always passes.
+
+    Raises:
+        AssertionError: If the assertion fails.
+    """
+    assert bool(1) is True
