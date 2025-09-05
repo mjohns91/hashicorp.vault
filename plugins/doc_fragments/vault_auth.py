aa1a1f8acadb409db3d95ef846a6ccbf6b16421e
Mandar Kulkarni <mandar242@gmail.com>
2025-09-05 11:50:53 -0700

    new module: kv2_secret and update delete kv2 secret client to handle multiple version deletion (#8)
    * add module for managing kv2 secret, update vault_client to delete multiple versions
    
    * add type hints
    
    * add initial integration tests
    
    * read vars from gh action secrets
    
    * minor fix
    
    * fix delete client and tests
    
    * update integration workflow to read vars from gh action secrets
    
    * add integration_config.yml template
    
    update integration workflow to read vars from gh action secrets
    
    update integration workflow to read vars from gh action secrets
    
    update integration workflow to read vars from gh action secrets
    
    update integration workflow to read vars from gh action secrets
    
    make render.sh executable
    
    * minor fix
    
    * add changelog fragment, linter fix
    
    * ansible-lint fix
    
    * add idempotency and tests
    
    * minor fix
    
    * black formatter fix
    
    * replace os with env_fallback
    
    * minor fixes
    
    * minor fix
    
    * fix test assertion
    
    * remove debug
    
    * add tests that use approle auth
    
    * update tests to delete secret permanently after test run
    
    update tests to delete secret permanently after test run
    
    * minor fixes
    
    * add RETURN block
    
    * disable changelog check, minor fixes in kv2_secret
    
    * doc fixes, remove changelog fragment
    
    * code optimization for kv2_secret
    
    * move authenticate_module, get_authenticated_client to module_utils for reuse
    
    * randomize test secret path to avoid collision
    
    * move token generation tasks to setup_auth_token, update requires_ansible version to >=2.16
    
    * restructure tests to reduce redundant code
    
    * read vars directly from integration_config.yml
    
    * update render integration_config.yml
    
    * update render integration_config.yml
    
    * remove render.sh
    
    * add check_mode support
    
    * black linter fixes
    
    * minor fixes
    
    * ansible-lint fix
    
    * minor fixes
    
    * sanity fix
    
    * move common auth params to doc_fragments
    
    * linter fix
    
    * remove hard coded changed values in exit_json
    
    * remove secret data from output
    
    * update description of return value

---
+++ plugins/doc_fragments/vault_auth.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2025 Red Hat, Inc.
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+
+
+__metaclass__ = type
+
+
+class ModuleDocFragment:
+    """Documentation fragment for HashiCorp Vault authentication options."""
+
+    # Common Vault authentication options
+    DOCUMENTATION = """
+options:
+  url:
+    description: Vault server URL.
+    required: true
+    type: str
+    aliases: [vault_address]
+  namespace:
+    description: Vault namespace.
+    default: admin
+    type: str
+    aliases: [vault_namespace]
+  auth_method:
+    description: Authentication method to use.
+    choices: ['token', 'approle']
+    default: token
+    type: str
+  token:
+    description:
+      - Vault token for authentication.
+      - Token can be provided as a parameter or as an environment variable E(VAULT_TOKEN).
+    type: str
+  role_id:
+    description:
+      - Role ID for AppRole authentication.
+      - AppRole O(role_id) can be provided as parameters or as environment variables E(VAULT_APPROLE_ROLE_ID).
+    type: str
+    aliases: [approle_role_id]
+  secret_id:
+    description:
+      - Secret ID for AppRole authentication.
+      - AppRole O(secret_id) can be provided as parameters or as environment variables E(VAULT_APPROLE_SECRET_ID).
+    type: str
+    aliases: [approle_secret_id]
+  vault_approle_path:
+    description: AppRole auth method mount path.
+    default: approle
+    type: str
+notes:
+  - Authentication is required for all Vault operations.
+  - Token authentication is the default method.
+  - For AppRole authentication, both O(role_id) and O(secret_id) are required.
+  - Environment variables take precedence over module parameters when both are provided.
+"""
