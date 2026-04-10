41a02277bec9d2ab0e3ba22e672fdffe8ae3479b
Bikouo Aubin <79859644+abikouo@users.noreply.github.com>
2026-04-10 13:37:33 +0200

    Update tests/unit/plugins/modules/test_pki_certificate.py
    Co-authored-by: Matthew Johnson <90540643+mjohns91@users.noreply.github.com>

--- tests/unit/plugins/modules/test_pki_certificate.py
+++ tests/unit/plugins/modules/test_pki_certificate.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 206 Red Hat, Inc.
+# Copyright (c) 2026 Red Hat, Inc.
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
